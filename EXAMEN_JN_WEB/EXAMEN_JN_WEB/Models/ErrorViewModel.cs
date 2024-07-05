c976ab654cdd206da164be15a5e70b56dfb7c2e0
Andrés Canul Bolaños <98436130+AndresCanul@users.noreply.github.com>
2024-07-04 18:05:00 -0600

    Inicio

---
+++ EXAMEN_JN_WEB/EXAMEN_JN_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EXAMEN_JN_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
